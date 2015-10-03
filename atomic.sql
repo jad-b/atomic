f77bd8bd2e2db6547edb8661a0ea9a4ddd1f7eac
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2015-10-03 18:55:43 -0400

    Create tables for work desc

--- atomic.sql
+++ atomic.sql
@@ -16,3 +16,27 @@ CREATE TABLE work (
     -- links
     priority int DEFAULT 0 CHECK (priority >= 0 and priority <= 10)
 );
+
+CREATE TABLE tag (
+    tag_id serial PRIMARY KEY,
+    tag text NOT NULL
+);
+
+/* Many-to-one relationship for tags to work */
+CREATE TABLE work_tag (
+    tag_id int REFERENCES tag (tag_id) ON UPDATE CASCADE,
+    work_id int REFERENCES work (work_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT tag_work_pkey PRIMARY KEY (tag_id, work_id)
+);
+
+CREATE TABLE work_lineage (
+    parent_id int REFERENCES work (work_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    child_id int REFERENCES work (work_id) ON UPDATE CASCADE,
+    CONSTRAINT parent_child_pkey PRIMARY KEY (parent_id, child_id)
+);
+
+CREATE TABLE work_link (
+    source_id int REFERENCES work (work_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    dest_id int REFERENCES work (work_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT source_dest_pkey PRIMARY KEY (source_id, dest_id)
+);
